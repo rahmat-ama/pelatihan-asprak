53431c35aa5de78df8f40a8f68a1ba0176c59a1d
Daffa Randika <daffa.randika.3@gmail.com>
2025-02-22 21:56:13 +0700

    soal4

---
+++ soal4/matrix.h
@@ -0,0 +1,29 @@
+#ifndef MATRIX_H
+#include <stdio.h>
+
+/*
+ * struct di bawah merepresentasikan
+ * matrix 2x2 dengan konfigurasi:
+ *
+ * [a b]
+ * [c d]
+*/
+
+typedef struct {
+	int a; int b;
+	int c; int d;
+} Matrix;
+
+Matrix newMatrix(int a, int b, int c, int d); 
+
+void printMatrix(Matrix m);
+
+Matrix add(Matrix m1, Matrix m2);
+
+Matrix subtract(Matrix m1, Matrix m2);
+
+void transpose(Matrix *m);
+
+int determinant(Matrix m);
+
+#endif 
